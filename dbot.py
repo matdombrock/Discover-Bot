e42dfd7f0f258874fa6d6682b36062810a4465e2
matdombrock <matdombrock@gmail.com>
2018-03-06 08:47:25 +0000

    added several new commands and updated help

--- dbot.py
+++ dbot.py
@@ -2,26 +2,43 @@
 #Mathieu Dombrock
 
 import discord
+import re
+import random
+import string
 import requests
-from pathlib import Path
+import json
+import pickle
 import glob
-client = discord.Client()
+from pathlib import Path
 
-bot_command = "?"
+client = discord.Client()
 
 @client.event
 async def on_message(message):
-	global bot_command
+	bot_command = "?"
+	memories = []
+	memories_file = 'memories.pk'
+	help_list = []
 	msgin_original = message.content #some functions need case sensitive input
 	msgin = message.content.lower()
 	msgin = msgin.split(" ")
 	msgin_original = msgin_original.split(" ")
 	print(msgin)
 	tag = '{0.author.mention} : \n'.format(message)
+
 	if message.author == client.user:#make sure we are not talking to ourselves 
 		return
+
+	if random.randrange(50)<2:
+		msgout = tag+"Thanks for being here!"
+		await client.send_message(message.channel, msgout)
+
+
+	help_list.append(["tutorial <topic>","Display a preset tutorial message."])
+	help_list.append(["tutorial list","List available tutorial topics."])
 	if (bot_command+'tutorial') in msgin[0] or (bot_command+'tut') in msgin[0]:
 		if ('list') in msgin[1]:
+			print("printing tut list")
 			tut_dir = 'tutorials/'
 			ftype = ".txt"
 			tut_list = (glob.glob(tut_dir+"*.txt"))
@@ -40,6 +57,83 @@ async def on_message(message):
 				#msgout += "\n \n[If you have suggestions for a new tutorial or concerns about this one, please let us know in #suggestions-and-meta]"
 			else:
 				msgout = tag+"I can't find any saved tutorials on "+msgin[1]+"... sorry :/"
+		await client.send_message(message.channel, msgout)
+
+	help_list.append(["test","See if I am working. Or more specifically, still responding to messages."])
+	if ((bot_command+'test') in msgin[0]):
+		msgout = "Yeah, yeah I, oh, I'm still alive."
+		await client.send_message(message.channel, msgout)
+
+	help_list.append(["roll <x>d<y>","Example: roll 1d3"])
+	if ((bot_command+'roll') in msgin[0]):
+		msgin = msgin[1].split("d")
+		result = ""
+		results = ""
+		for x in range(int(msgin[0])):
+			result = random.randrange( 0, int(msgin[1]) ) +1
+			results += str(result)+"\n"
+		msgout = tag+results
+		await client.send_message(message.channel, msgout)
+
+	help_list.append(["choose <choice1> <choice2> <choice3> <...>","Helps you make a choice. \nExample: choose Debian Ubuntu CentOS\nOutput: Ubuntu"])
+	if ((bot_command+'choose') in msgin[0]):
+		print("choose")
+		msgin = msgin[1:]
+		print(msgin)
+		msgout = tag+"Choosing from: "+', '.join(msgin)+"\n"+random.choice(msgin)
+		await client.send_message(message.channel, msgout)
+
+	help_list.append(["google <search-term>","I'm feeling lucky"])
+	if ((bot_command+'google') in msgin[0]):
+		r = requests.get("http://www.google.com/search?q="+' '.join(msgin[1:])+"&btnI");
+		msgout = r.url
+		msgout = tag+str(msgout)
+		await client.send_message(message.channel, msgout)
+
+	help_list.append(["wiki <search-term>","searches wikipedia"])
+	if ((bot_command+'wiki') in msgin[0]):
+		r = requests.get("https://en.wikipedia.org/w/api.php?action=opensearch&search="+' '.join(msgin[1:])+"&limit=1&namespace=0&format=json");
+		msgout = json.loads(r.text)
+		msgout = tag+str(msgout[3]).strip("[\"']")
+		await client.send_message(message.channel, msgout)
+
+	help_list.append(["remember <topic/key> <memory/value>","Remember the following\nExample: remember Dre hip-hop mastermind"])
+	if ((bot_command+'remember') in msgin[0]):
+		subject = msgin[1]
+		memory = ' '.join(msgin_original[2:])
+		msgout = tag+'Remembered '+subject+" as "+memory
+		memories.append([subject, memory])
+		#open a pickle file
+		with open(memories_file, 'wb') as fi:
+		# dump your data into the file
+			pickle.dump(memories, fi)
+		await client.send_message(message.channel, msgout)
+
+	help_list.append(["recall <topic/key>","recall a memory\nExample: recall Dre"])
+	if ((bot_command+'recall') in msgin[0]):
+		with open(memories_file, 'rb') as fi:
+			memories = pickle.load(fi)
+		found = False
+		for x in memories:
+			if x[0] == msgin[1]:
+				found = True
+				subject = x[0]
+				memory = x[1]
+				msgout = tag+"I Recall "+subject+" as "+memory
+		if found == False:
+			msgout = tag+"I don't recall that."
+		await client.send_message(message.channel, msgout)
+
+
+	#help must be last to ensure the help list is loaded
+	help_list.append(["help","Displays Help Message."])
+	if ((bot_command+'help') in msgin[0]):
+		msgout = "```CSS\nDiscover-Bot```"
+		for x in sorted(help_list):
+			msgout += "```\n"
+			msgout += bot_command+'\n'.join(x).replace("\n", "\n--")+"\n\n"
+			msgout += "\n```"
+		msgout += "\nhttps://github.com/matdombrock/Discover-Bot\n"
 		await client.send_message(message.channel, msgout)
 
 
